60a2f02cad1acece43dcbcd4ed93e692f8a8639d
Andre Sutherland <andrelsf95@gmail.com>
2025-06-19 00:12:26 +0100

    feat: add code signing config resource implementation

---
+++ services/lambda/code_signing_config_resource_destroy_test.go
@@ -0,0 +1,130 @@
+package lambda
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	"github.com/newstack-cloud/celerity-provider-aws/internal/testutils"
+	"github.com/newstack-cloud/celerity-provider-aws/utils"
+	"github.com/newstack-cloud/celerity/libs/blueprint/core"
+	"github.com/newstack-cloud/celerity/libs/blueprint/provider"
+	"github.com/newstack-cloud/celerity/libs/blueprint/state"
+	"github.com/newstack-cloud/celerity/libs/plugin-framework/sdk/plugintestutils"
+	"github.com/stretchr/testify/suite"
+)
+
+type LambdaCodeSigningConfigResourceDestroySuite struct {
+	suite.Suite
+}
+
+func (s *LambdaCodeSigningConfigResourceDestroySuite) Test_destroy() {
+	loader := &testutils.MockAWSConfigLoader{}
+	providerCtx := plugintestutils.NewTestProviderContext(
+		"aws",
+		map[string]*core.ScalarValue{
+			"region": core.ScalarFromString("us-west-2"),
+		},
+		map[string]*core.ScalarValue{
+			"session_id": core.ScalarFromString("test-session-id"),
+		},
+	)
+
+	testCases := []plugintestutils.ResourceDestroyTestCase[*aws.Config, Service]{
+		createSuccessfulCodeSigningConfigDestroyTestCase(providerCtx, loader),
+		createFailingCodeSigningConfigDestroyTestCase(providerCtx, loader),
+	}
+
+	plugintestutils.RunResourceDestroyTestCases(
+		testCases,
+		CodeSigningConfigResource,
+		&s.Suite,
+	)
+}
+
+func createSuccessfulCodeSigningConfigDestroyTestCase(
+	providerCtx provider.Context,
+	loader *testutils.MockAWSConfigLoader,
+) plugintestutils.ResourceDestroyTestCase[*aws.Config, Service] {
+	service := createLambdaServiceMock(
+		WithDeleteCodeSigningConfigOutput(&lambda.DeleteCodeSigningConfigOutput{}),
+	)
+
+	expectedCodeSigningConfigARN := "arn:aws:lambda:us-east-1:123456789012:code-signing-config:test-config"
+
+	return plugintestutils.ResourceDestroyTestCase[*aws.Config, Service]{
+		Name: "successfully deletes code signing config",
+		ServiceFactory: func(awsConfig *aws.Config, providerContext provider.Context) Service {
+			return service
+		},
+		ServiceMockCalls: &service.MockCalls,
+		ConfigStore: utils.NewAWSConfigStore(
+			[]string{},
+			utils.AWSConfigFromProviderContext,
+			loader,
+			utils.AWSConfigCacheKey,
+		),
+		Input: &provider.ResourceDestroyInput{
+			ProviderContext: providerCtx,
+			ResourceState: &state.ResourceState{
+				SpecData: &core.MappingNode{
+					Fields: map[string]*core.MappingNode{
+						"codeSigningConfigArn": core.MappingNodeFromString(expectedCodeSigningConfigARN),
+					},
+				},
+			},
+		},
+		ExpectError: false,
+		DestroyActionsCalled: map[string]any{
+			"DeleteCodeSigningConfig": &lambda.DeleteCodeSigningConfigInput{
+				CodeSigningConfigArn: aws.String(expectedCodeSigningConfigARN),
+			},
+		},
+	}
+}
+
+func createFailingCodeSigningConfigDestroyTestCase(
+	providerCtx provider.Context,
+	loader *testutils.MockAWSConfigLoader,
+) plugintestutils.ResourceDestroyTestCase[*aws.Config, Service] {
+	service := createLambdaServiceMock(
+		WithDeleteCodeSigningConfigError(errors.New("failed to delete code signing config")),
+	)
+
+	expectedCodeSigningConfigARN := "arn:aws:lambda:us-east-1:123456789012:code-signing-config:test-config"
+
+	return plugintestutils.ResourceDestroyTestCase[*aws.Config, Service]{
+		Name: "fails to delete code signing config",
+		ServiceFactory: func(awsConfig *aws.Config, providerContext provider.Context) Service {
+			return service
+		},
+		ServiceMockCalls: &service.MockCalls,
+		ConfigStore: utils.NewAWSConfigStore(
+			[]string{},
+			utils.AWSConfigFromProviderContext,
+			loader,
+			utils.AWSConfigCacheKey,
+		),
+		Input: &provider.ResourceDestroyInput{
+			ProviderContext: providerCtx,
+			ResourceState: &state.ResourceState{
+				SpecData: &core.MappingNode{
+					Fields: map[string]*core.MappingNode{
+						"codeSigningConfigArn": core.MappingNodeFromString(expectedCodeSigningConfigARN),
+					},
+				},
+			},
+		},
+		ExpectError: true,
+		DestroyActionsCalled: map[string]any{
+			"DeleteCodeSigningConfig": &lambda.DeleteCodeSigningConfigInput{
+				CodeSigningConfigArn: aws.String(expectedCodeSigningConfigARN),
+			},
+		},
+	}
+}
+
+func TestLambdaCodeSigningConfigResourceDestroySuite(t *testing.T) {
+	suite.Run(t, new(LambdaCodeSigningConfigResourceDestroySuite))
+}
---
+++ services/lambda/code_signing_config_resource_get_external_state.go
@@ -0,0 +1,156 @@
+package lambda
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	"github.com/newstack-cloud/celerity/libs/blueprint/core"
+	"github.com/newstack-cloud/celerity/libs/blueprint/provider"
+	"github.com/newstack-cloud/celerity/libs/plugin-framework/sdk/pluginutils"
+)
+
+func (l *lambdaCodeSigningConfigResourceActions) GetExternalState(
+	ctx context.Context,
+	input *provider.ResourceGetExternalStateInput,
+) (*provider.ResourceGetExternalStateOutput, error) {
+	lambdaService, err := l.getLambdaService(ctx, input.ProviderContext)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Lambda service: %w", err)
+	}
+
+	codeSigningConfigArn := core.StringValue(
+		input.CurrentResourceSpec.Fields["codeSigningConfigArn"],
+	)
+
+	getCodeSigningConfigInput := &lambda.GetCodeSigningConfigInput{
+		CodeSigningConfigArn: aws.String(codeSigningConfigArn),
+	}
+
+	result, err := lambdaService.GetCodeSigningConfig(ctx, getCodeSigningConfigInput)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Lambda code signing config: %w", err)
+	}
+
+	// Build resource spec state from AWS response
+	resourceSpecState := l.buildBaseResourceSpecState(result)
+
+	// Add optional fields if they exist
+	err = l.addOptionalConfigurationsToSpec(result, resourceSpecState.Fields)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get tags
+	listTagsInput := &lambda.ListTagsInput{
+		Resource: aws.String(codeSigningConfigArn),
+	}
+	tagsOutput, err := lambdaService.ListTags(ctx, listTagsInput)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Lambda code signing config tags: %w", err)
+	}
+
+	// Add tags if present
+	if tagsOutput != nil && len(tagsOutput.Tags) > 0 {
+		tagNodes := make([]*core.MappingNode, 0, len(tagsOutput.Tags))
+		for key, value := range tagsOutput.Tags {
+			tagNodes = append(tagNodes, &core.MappingNode{
+				Fields: map[string]*core.MappingNode{
+					"key":   core.MappingNodeFromString(key),
+					"value": core.MappingNodeFromString(value),
+				},
+			})
+		}
+		resourceSpecState.Fields["tags"] = &core.MappingNode{
+			Items: tagNodes,
+		}
+	}
+
+	return &provider.ResourceGetExternalStateOutput{
+		ResourceSpecState: resourceSpecState,
+	}, nil
+}
+
+func (l *lambdaCodeSigningConfigResourceActions) buildBaseResourceSpecState(
+	output *lambda.GetCodeSigningConfigOutput,
+) *core.MappingNode {
+	return &core.MappingNode{
+		Fields: map[string]*core.MappingNode{
+			"codeSigningConfigArn": core.MappingNodeFromString(
+				aws.ToString(output.CodeSigningConfig.CodeSigningConfigArn),
+			),
+			"codeSigningConfigId": core.MappingNodeFromString(
+				aws.ToString(output.CodeSigningConfig.CodeSigningConfigId),
+			),
+		},
+	}
+}
+
+func (l *lambdaCodeSigningConfigResourceActions) addOptionalConfigurationsToSpec(
+	output *lambda.GetCodeSigningConfigOutput,
+	specFields map[string]*core.MappingNode,
+) error {
+	extractors := []pluginutils.OptionalValueExtractor[*lambda.GetCodeSigningConfigOutput]{
+		{
+			Name: "description",
+			Condition: func(output *lambda.GetCodeSigningConfigOutput) bool {
+				return output.CodeSigningConfig.Description != nil
+			},
+			Fields: []string{"description"},
+			Values: func(output *lambda.GetCodeSigningConfigOutput) ([]*core.MappingNode, error) {
+				return []*core.MappingNode{
+					core.MappingNodeFromString(aws.ToString(output.CodeSigningConfig.Description)),
+				}, nil
+			},
+		},
+		{
+			Name: "allowedPublishers",
+			Condition: func(output *lambda.GetCodeSigningConfigOutput) bool {
+				return output.CodeSigningConfig.AllowedPublishers != nil
+			},
+			Fields: []string{"allowedPublishers"},
+			Values: func(output *lambda.GetCodeSigningConfigOutput) ([]*core.MappingNode, error) {
+				return []*core.MappingNode{
+					{
+						Fields: map[string]*core.MappingNode{
+							"signingProfileVersionArns": buildSigningProfileVersionArnsNode(
+								output.CodeSigningConfig.AllowedPublishers.SigningProfileVersionArns,
+							),
+						},
+					},
+				}, nil
+			},
+		},
+		{
+			Name: "codeSigningPolicies",
+			Condition: func(output *lambda.GetCodeSigningConfigOutput) bool {
+				return output.CodeSigningConfig.CodeSigningPolicies != nil
+			},
+			Fields: []string{"codeSigningPolicies"},
+			Values: func(output *lambda.GetCodeSigningConfigOutput) ([]*core.MappingNode, error) {
+				codeSigningPolicies := &core.MappingNode{
+					Fields: map[string]*core.MappingNode{},
+				}
+				if output.CodeSigningConfig.CodeSigningPolicies.UntrustedArtifactOnDeployment != "" {
+					codeSigningPolicies.Fields["untrustedArtifactOnDeployment"] = core.MappingNodeFromString(
+						string(output.CodeSigningConfig.CodeSigningPolicies.UntrustedArtifactOnDeployment),
+					)
+				}
+				return []*core.MappingNode{codeSigningPolicies}, nil
+			},
+		},
+	}
+
+	return pluginutils.RunOptionalValueExtractors(output, specFields, extractors)
+}
+
+func buildSigningProfileVersionArnsNode(arns []string) *core.MappingNode {
+	arnNodes := make([]*core.MappingNode, len(arns))
+	for i, arn := range arns {
+		arnNodes[i] = core.MappingNodeFromString(arn)
+	}
+	return &core.MappingNode{
+		Items: arnNodes,
+	}
+}
