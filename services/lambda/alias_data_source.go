d115849470f83574f5e8b0c41a4a1b68a4700fc5
Andre Sutherland <andrelsf95@gmail.com>
2025-06-19 01:41:11 +0100

    chore: exclude schemas from code duplication analysis
    Schemas are best read as a block in one place instead of being broken down into functions
    purely for the purpose of code reuse. The string values will be unique for all schema elements
    so the savings from code reuse will be a couple of lines for each element that defines
    the type and whether or not the element is nullable, which are easier to read when they
    are clearly set in the schema definition block.

--- services/lambda/alias_data_source.go
+++ services/lambda/alias_data_source.go
@@ -59,53 +59,6 @@ func AliasDataSource(
 	}
 }
 
-func lambdaAliasDataSourceSchema() map[string]*provider.DataSourceSpecSchema {
-	return map[string]*provider.DataSourceSpecSchema{
-		"arn": {
-			Label:       "Alias ARN",
-			Type:        provider.DataSourceSpecTypeString,
-			Description: "The ARN of the Lambda alias.",
-			Nullable:    false,
-		},
-		"description": {
-			Label:       "Description",
-			Type:        provider.DataSourceSpecTypeString,
-			Description: "Description of the alias.",
-			Nullable:    true,
-		},
-		"functionName": {
-			Label:       "Function Name",
-			Type:        provider.DataSourceSpecTypeString,
-			Description: "The name of the Lambda function.",
-			Nullable:    false,
-		},
-		"functionVersion": {
-			Label:       "Function Version",
-			Type:        provider.DataSourceSpecTypeString,
-			Description: "The function version that the alias invokes.",
-			Nullable:    false,
-		},
-		"invokeArn": {
-			Label:       "Invoke ARN",
-			Type:        provider.DataSourceSpecTypeString,
-			Description: "The invocation ARN of the alias to be used when invoking from API Gateway.",
-			Nullable:    false,
-		},
-		"name": {
-			Label:       "Alias Name",
-			Type:        provider.DataSourceSpecTypeString,
-			Description: "The name of the alias.",
-			Nullable:    false,
-		},
-		"routingConfig.additionalVersionWeights": {
-			Label:       "Additional Version Weights",
-			Type:        provider.DataSourceSpecTypeString,
-			Description: "A JSON-encoded string of the additional version weights for the alias.",
-			Nullable:    true,
-		},
-	}
-}
-
 type lambdaAliasDataSourceFetcher struct {
 	lambdaServiceFactory pluginutils.ServiceFactory[*aws.Config, Service]
 	awsConfigStore       pluginutils.ServiceConfigStore[*aws.Config]
---
+++ services/lambda/alias_data_source_schema.go
@@ -0,0 +1,50 @@
+package lambda
+
+import "github.com/newstack-cloud/celerity/libs/blueprint/provider"
+
+func lambdaAliasDataSourceSchema() map[string]*provider.DataSourceSpecSchema {
+	return map[string]*provider.DataSourceSpecSchema{
+		"arn": {
+			Label:       "Alias ARN",
+			Type:        provider.DataSourceSpecTypeString,
+			Description: "The ARN of the Lambda alias.",
+			Nullable:    false,
+		},
+		"description": {
+			Label:       "Description",
+			Type:        provider.DataSourceSpecTypeString,
+			Description: "Description of the alias.",
+			Nullable:    true,
+		},
+		"functionName": {
+			Label:       "Function Name",
+			Type:        provider.DataSourceSpecTypeString,
+			Description: "The name of the Lambda function.",
+			Nullable:    false,
+		},
+		"functionVersion": {
+			Label:       "Function Version",
+			Type:        provider.DataSourceSpecTypeString,
+			Description: "The function version that the alias invokes.",
+			Nullable:    false,
+		},
+		"invokeArn": {
+			Label:       "Invoke ARN",
+			Type:        provider.DataSourceSpecTypeString,
+			Description: "The invocation ARN of the alias to be used when invoking from API Gateway.",
+			Nullable:    false,
+		},
+		"name": {
+			Label:       "Alias Name",
+			Type:        provider.DataSourceSpecTypeString,
+			Description: "The name of the alias.",
+			Nullable:    false,
+		},
+		"routingConfig.additionalVersionWeights": {
+			Label:       "Additional Version Weights",
+			Type:        provider.DataSourceSpecTypeString,
+			Description: "A JSON-encoded string of the additional version weights for the alias.",
+			Nullable:    true,
+		},
+	}
+}
